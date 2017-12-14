9ea4c1e7ae9da063cf9790f8832b4fe02d8e0e74
Tomas Jedlicka <jedlickat@gmail.com>
2017-12-14 09:30:08 -0500

    Build a slice containig all probes

--- probe.go
+++ probe.go
@@ -5,55 +5,44 @@ package dtrace
 // int addProbe_cgo(dtrace_hdl_t *h, const dtrace_probedesc_t *pd, void *arg);
 import "C"
 import (
-	"fmt"
 	"unsafe"
 )
 
 // Probe description
 type Probe struct {
-	dpProbeDesc *C.dtrace_probedesc_t
+	ProbeID		int
+	ProviderName	string
+	ModuleName	string
+	FunctionName	string
+	ProbeName	string
 }
 
-// GetID returns probe ID
-func (probe *Probe) GetID() int {
-	return int(probe.dpProbeDesc.dtpd_id)
-}
-
-// GetProviderName returns provider name
-func (probe *Probe) GetProviderName() string {
-	return C.GoString(&probe.dpProbeDesc.dtpd_provider[0])
-}
-
-// GetModuleName returns module name
-func (probe *Probe) GetModuleName() string {
-	return C.GoString(&probe.dpProbeDesc.dtpd_mod[0])
-}
-
-// GetFuncName returns function name
-func (probe *Probe) GetFuncName() string {
-	return C.GoString(&probe.dpProbeDesc.dtpd_func[0])
-}
-
-// GetProbeName returns probe name
-func (probe *Probe) GetProbeName() string {
-	return C.GoString(&probe.dpProbeDesc.dtpd_name[0])
-}
+//
+// For the moment we use global array to build a list of probes
+// until we build something better to support parallel iterations.
+var probes []Probe
 
 //export addProbe
 func addProbe(handle *C.dtrace_hdl_t, pdesc *C.dtrace_probedesc_t,
 	arg *C.char) int {
-	probe := Probe{dpProbeDesc: pdesc}
-	fmt.Printf("%d: %s: %s: %s: %s\n",
-		probe.GetID(), probe.GetProviderName(), probe.GetModuleName(),
-		probe.GetFuncName(), probe.GetProbeName())
+	probe := Probe{
+		ProbeID:	int(pdesc.dtpd_id),
+		ProviderName:	C.GoString(&pdesc.dtpd_provider[0]),
+		ModuleName:	C.GoString(&pdesc.dtpd_mod[0]),
+		FunctionName:	C.GoString(&pdesc.dtpd_func[0]),
+		ProbeName:	C.GoString(&pdesc.dtpd_name[0]),
+	}
+	probes = append(probes, probe)
 	return 0
 }
 
 // GetProbes return
 func GetProbes(handle *DTraceHandle) []Probe {
+	probes = make([]Probe, 1)
+
 	// Iterate over all probes and fill in the array
 	C.dtrace_probe_iter(handle.dhLibHandle, nil,
 		(*C.dtrace_probe_f)(unsafe.Pointer(C.addProbe_cgo)), nil)
 
-	return nil
+	return probes
 }
